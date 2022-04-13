4770270e3c827aca33b07531a379b831a3c084a3
rohinihire <91669305+rohinihire@users.noreply.github.com>
2022-04-13 09:34:26 +0530

    Update hello.js

--- src/modules/recipe/hello/hello.js
+++ src/modules/recipe/hello/hello.js
@@ -2,4 +2,8 @@ import { LightningElement } from 'lwc';
 
 export default class Hello extends LightningElement {
     greeting = 'World';
+    
+    handleChange(event){
+      this.greeting = event.target.value;   
+    }
 }
